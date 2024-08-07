a5ca1470606e9530098aa76e1278590b0c555f74
Emanuel Silva <enrique.silva@ppro.com>
2024-08-07 09:30:30 -0300

    Adiciona configuracao do autojump para o ZSH

--- ZSH/init.sh
+++ ZSH/init.sh
@@ -2,10 +2,12 @@
 
 ZSH_HOME=$HOME/.zsh
 CONFIG_HOME=$HOME/.config
+AUTOJUMP_CONFIG=/Library/autojump
 
 mkdir -p $ZSH_HOME
 mkdir -p $CONFIG_HOME
 
+brew install autojump
 brew install starship
 brew install eza
 brew install autojump
@@ -15,3 +17,4 @@ brew install zsh-syntax-highlighting
 cp .zshrc $HOME
 cp aliases $ZSH_HOME
 cp starship.toml $CONFIG_HOME
+cp autojump.txt* $AUTOJUMP_CONFIG
